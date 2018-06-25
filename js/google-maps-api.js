084bcee6423d9813cd2fdc7a48ffc301a26b91b9
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-06-25 15:24:18 -0500

    damn buttons are done... fuck

--- js/google-maps-api.js
+++ js/google-maps-api.js
@@ -1,30 +1,36 @@
 (function () {
     "use strict";
 
-    var mapOptions = {
-        zoom: 10,
+    function renderMap(zoomLevel) {
+        var mapOptions = {
+            zoom: zoomLevel,
 
-        center: {
-            lat: 29.426791,
-            lng: -98.489602
-        }
-    };
+            center: {
+                lat: 29.426791,
+                lng: -98.489602
+            }
+        };
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    var geo = new google.maps.Geocoder();
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    geo.geocode({address: "105 N Interstate 35 San Marcos, TX 78666"}, function (response, status) {
-        console.log(status);
-        map.setCenter(response[0].geometry.location);
-    });
+        var geo = new google.maps.Geocoder();
 
-    var listener = function (event) {
-        // alert("Zoom 5");
-            mapOptions.zoom = 5;
+        geo.geocode({address: "105 N Interstate 35 San Marcos, TX 78666"}, function (response, status) {
+            console.log(status);
+            map.setCenter(response[0].geometry.location);
+        });
+    }
 
-    };
+    renderMap(10);
 
-    var zoom5Button = document.getElementById("zoom-5-button");
-    zoom5Button.addEventListener('click', listener, false);
+    var zoomButton = document.getElementsByClassName("zoom-button");
+    console.table(zoomButton);
+    // creates event listener for each button
+    for (var i = 0; i < zoomButton.length ; i++) {
+        zoomButton[i].addEventListener('click', function () {
+            renderMap(parseInt(this.value));
+            console.log(this.value);
+        }, false);
+    }
 
 })();
