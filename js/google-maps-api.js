58daf9d083dc4418bb0c1e2ffb0d6fafe9fd71fe
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-06-25 13:50:45 -0500

    made buttons and fixed map

--- js/google-maps-api.js
+++ js/google-maps-api.js
@@ -1,25 +1,22 @@
-(function() {
+(function () {
     "use strict";
 
     var mapOptions = {
         zoom: 10,
 
         center: {
-            lat:  29.426791,
+            lat: 29.426791,
             lng: -98.489602
         }
     };
 
-    function initMap() {
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var geo = new google.maps.Geocoder();
 
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        var geo = new google.maps.Geocoder();
-
-        geo.geocode({address: "1 UTSA Circle"}, function (response, status) {
-            console.log(status);
-            map.setCenter(response[0].geometry.location);
-        })
-    }
+    geo.geocode({address: "105 N Interstate 35 San Marcos, TX 78666"}, function (response, status) {
+        console.log(status);
+        map.setCenter(response[0].geometry.location);
+    })
 
 
 })();
